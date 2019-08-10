2a3d0e59dab4cd9e98765c0cee96b426ca02d73f
chiron <ios.chiron@gmail.com>
2019-08-11 04:38:50 +0800

    feature: add Apple Pie Project for Unit2

---
+++ Unit2/Apple Pie/Apple Pie/Game.swift
@@ -0,0 +1,34 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by mikewang on 2019/8/11.
+//  Copyright © 2019 chiron. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    mutating func playerGussed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+}

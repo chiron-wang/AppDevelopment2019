57165488073d289c3734b073d43ac1c0a7b3a607
chiron <ios.chiron@gmail.com>
2019-08-13 23:33:18 +0800

    Lab: add RainbowTabs for Unit4

---
+++ Unit4/RainbowTabs/RainbowTabs/OrangeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  RainbowTabs
+//
+//  Created by mikewang on 2019/8/13.
+//  Copyright © 2019 chiron. All rights reserved.
+//
+
+import UIKit
+
+class OrangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tabBarItem.badgeValue = "!"
+    }
+
+
+}
+

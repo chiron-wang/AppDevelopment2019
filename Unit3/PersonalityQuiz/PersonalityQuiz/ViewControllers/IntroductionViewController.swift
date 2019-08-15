98f0f85b77cde526f2e152b7db8ce524c24a1dc5
chiron <ios.chiron@gmail.com>
2019-08-15 14:49:25 +0800

    Lab: add PersonalityQuiz for Unit3

---
+++ Unit3/PersonalityQuiz/PersonalityQuiz/ViewControllers/IntroductionViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by mikewang on 2019/8/15.
+//  Copyright © 2019 chiron. All rights reserved.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue) {
+        
+    }
+
+}
+
---
+++ Unit3/PersonalityQuiz/PersonalityQuiz/ViewControllers/ResultsViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ResultsViewController.swift
+//  PersonalityQuiz
+//
+//  Created by mikewang on 2019/8/15.
+//  Copyright © 2019 chiron. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    @IBOutlet weak var resultAnswerLabel: UILabel!
+    @IBOutlet weak var resultDefinitionLabel: UILabel!
+    var responses: [Answer]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+        calculatePersonalityResult()
+    }
+    
+    func calculatePersonalityResult() {
+        var frequencyOfAnswers: [AnimalType:Int] = [:]
+        let responseTypes = responses.map { $0.type }
+        
+        for response in responseTypes {
+            let newCount: Int
+            
+            if let oldCount = frequencyOfAnswers[response] {
+                newCount = oldCount + 1
+            } else {
+                newCount = 1
+            }
+            
+            frequencyOfAnswers[response] = newCount
+        }
+        
+        let mostCommonAnswer = frequencyOfAnswers.sorted { $0.1 > $1.1 }.first!.key
+        
+        resultAnswerLabel.text = "You are a \(mostCommonAnswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonAnswer.difinition
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

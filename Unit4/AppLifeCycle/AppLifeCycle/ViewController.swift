0699d8f3609a1a6b56d8ce258dede3c3ba8c17e5
chiron <ios.chiron@gmail.com>
2019-08-15 18:42:02 +0800

    Lab: add App Life Cycle for Unit4

---
+++ Unit4/AppLifeCycle/AppLifeCycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppLifeCycle
+//
+//  Created by mikewang on 2019/8/15.
+//  Copyright © 2019 chiron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
